5a3cda6a2bfb41868ae28a9779fad852d27c0bec
jshuntley <josh@jshuntley.com>
2025-03-19 08:24:23 -0700

    update bootstrapper

--- config-bootstrap.sh
+++ config-bootstrap.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Dotfile bootstrapper for Linux systems because I'm lazy
+# Personalized app and dotfile bootstrapper for Linux systems
 
 # Update
 sudo apt update && sudo apt upgrade -y
@@ -9,8 +9,8 @@ sudo apt update && sudo apt upgrade -y
 sudo apt install zsh
 
 # configure .zshrc
-mv ~/.zshrc ~/.zshrc.bak
-wget https://raw.githubusercontent.com/jshuntley/dotfiles/refs/heads/main/.zshrc -O ~/.zshrc
+mv $HOME/.zshrc $HOME/.zshrc.bak
+wget https://raw.githubusercontent.com/jshuntley/dotfiles/refs/heads/main/.zshrc -O $HOME/.zshrc
 
 # set shell to zsh
 sudo chsh -s $(which zsh) $(whoami)
@@ -18,26 +18,28 @@ sudo chsh -s $(which zsh) $(whoami)
 # install rust
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 
-# install Starship
+# install starship
 curl -sS https://starship.rs/install.sh | sh
-wget https://raw.githubusercontent.com/jshuntley/dotfiles/refs/heads/main/.config/starship.toml -O ~/.config/starship.toml
-
-source ~/.zshrc
-
-# install zoxide
-cargo install zoxide
+wget https://raw.githubusercontent.com/jshuntley/dotfiles/refs/heads/main/.config/starship.toml -O $HOME/.config/starship.toml
 
 # install homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+source $HOME/.zshrc
 
 # install brew packages & fonts
 brew install helix eza lazygit
 brew install --cask font-hack-nerd-font font-meslo-lg-nerd-font
 
 # configure helix
-wget https://raw.githubusercontent.com/jshuntley/dotfiles/refs/heads/main/.config/helix/config.toml -O ~/.config/helix/config.toml
+wget https://raw.githubusercontent.com/jshuntley/dotfiles/refs/heads/main/.config/helix/config.toml -O $HOME/.config/helix/config.toml
+
+# install zoxide
+cargo install zoxide
 
 # install and configure Kitty
 curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin
+ln -s $HOME/.local/kitty.app/bin/kitty $HOME/.local/bin/.
+ln -s $HOME/.local/kitty.app/bin/kitten $HOME/.local/bin/.
 
-# echo "" > ~/.config/kitty/
+# echo "" > $HOME/.config/kitty/
